c4b83644619fbc42ba7bdaf2eb628f3858c37b05
René Hoffmann <hoffelmann@gmail.com>
2021-01-12 17:06:55 +0000

    working like a charm

--- light_dateTime.sh
+++ light_dateTime.sh
@@ -2,7 +2,7 @@
 
 ## [ Parameters ] ##############################################################
 gpio_pin_light=17
-path_table_date_dawnDuskTime=$(dirname $0)/date_dawnDuskTime.csv
+path_table_date_dawnDuskTime=$(dirname $0)/table_date_dawnDuskTime.csv
 
 
 ## [ Logic ] ##################################################################
@@ -36,25 +36,25 @@ fi
 echo
 
 while true; do
-    _date=$(date +%m%d)
-    _time=$(date +%H%M)
-    _dawn=$(grep "${_date}" table_date_dawnDuskTime.csv | cut -d',' -f2)
-    _dusk=$(grep "${_date}" table_date_dawnDuskTime.csv | cut -d',' -f3)
+    _date=$(date "+%m%d")
+    _time=$(date "+%H%M")
+    _dawn=$(grep "${_date}" "${path_table_date_dawnDuskTime}" | cut -d',' -f2 || { echo "Date \"${_date}\" not found in ${path_table_date_dawnDuskTime}"; exit 1; })
+    _dusk=$(grep "${_date}" "${path_table_date_dawnDuskTime}" | cut -d',' -f3 || { echo "Date \"${_date}\" not found in ${path_table_date_dawnDuskTime}"; exit 1; })
 
-    if [ ${_time} -ge ${_dawn} ] && [ ${_time} -le ${_dusk} ]; then
-        echo "0" > /sys/class/gpio/gpio${gpio_pin_light}/value
-        if [ ${toggle:=0} -eq 1 ]; then
-            echo "Switch fairy light off"
-            systemd-cat --identifier ${0} --priority info echo "Switch fairy light off"
-        fi
-        toggle=0
-    else
+    if [ "${_time}" -ge "${_dusk}" ] || [ "${_time}" -le "${_dawn}" ]; then
         echo "1" > /sys/class/gpio/gpio${gpio_pin_light}/value
         if [ ${toggle:=0} -eq 0 ]; then
             echo "Switch fairy light on"
             systemd-cat --identifier ${0} --priority info echo "Switch fairy light on"
         fi
         toggle=1
+    else
+        echo "0" > /sys/class/gpio/gpio${gpio_pin_light}/value
+        if [ ${toggle:=0} -eq 1 ]; then
+            echo "Switch fairy light off"
+            systemd-cat --identifier ${0} --priority info echo "Switch fairy light off"
+        fi
+        toggle=0
     fi
 
     sleep 1m
